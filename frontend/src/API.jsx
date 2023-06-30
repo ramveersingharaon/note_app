3f9dae5c906c521d104b71c845c641f4b739654f
ramveersingharaon <ramveersingharaon@gmail.com>
2023-07-01 01:50:42 +0530

    after deploy

--- frontend/src/API.jsx
+++ frontend/src/API.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API = axios.create({ baseURL: "http://localhost:8080" });
+const API = axios.create({ baseURL: "https://note-app-backend-new.onrender.com/" });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('token')) {
